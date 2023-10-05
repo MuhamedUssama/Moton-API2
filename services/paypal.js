ff42a9f6c467fd180030f3c4acd23e3f01cbd955
Mohamed <mohamed.osama200538@gmail.com>
2023-10-05 12:46:54 +0300

    Change paypal url

--- services/paypal.js
+++ services/paypal.js
@@ -1,9 +1,9 @@
-const paypal = require('paypal-rest-sdk');
+const paypal = require("paypal-rest-sdk");
 
 paypal.configure({
-  'mode': 'sandbox', //sandbox or live
-  'client_id': process.env.PAYPAL_CLIENT_ID,
-  'client_secret': process.env.PAYPAL_CLIENT_SECRET
+  mode: "sandbox", //sandbox or live
+  client_id: process.env.PAYPAL_CLIENT_ID,
+  client_secret: process.env.PAYPAL_CLIENT_SECRET,
 });
 
 const createAsyncPayment = (create_payment_json) => {
@@ -13,48 +13,48 @@ const createAsyncPayment = (create_payment_json) => {
         reject(error);
       } else {
         for (let i = 0; i < payment.links.length; i++) {
-          if (payment.links[i].rel === 'approval_url') {
+          if (payment.links[i].rel === "approval_url") {
             resolve(payment.links[i].href);
           }
         }
       }
     });
   });
-}
+};
 
 exports.createPaypalPayment = async (totalPrice, currency, cartId) => {
   try {
     const create_payment_json = {
-      "intent": "sale",
-      "payer": {
-        "payment_method": "paypal"
+      intent: "sale",
+      payer: {
+        payment_method: "paypal",
       },
-      "redirect_urls": {
+      redirect_urls: {
         // "return_url": "http://localhost:3000/success",
         // "cancel_url": "http://localhost:3000/cancel"
-        "return_url": "https://bookstore-iti.herokuapp.com/success",
-        "cancel_url": "https://bookstore-iti.herokuapp.com/cancel"
+        return_url: "https://moton.vercel.app/paypal/payment/success",
+        cancel_url: "https://bookstore-iti.herokuapp.com/cancel",
       },
-      "transactions": [{
-        "amount": {
-          "currency": currency,
-          "total": totalPrice + 12,
+      transactions: [
+        {
+          amount: {
+            currency: currency,
+            total: totalPrice + 12,
+          },
+          description: cartId,
         },
-        "description": cartId
-      }]
+      ],
     };
 
     const paymentUrl = await createAsyncPayment(create_payment_json);
     return paymentUrl;
   } catch (e) {
-    console.log(e)
+    console.log(e);
   }
-}
+};
 
 exports.executePaypalPayment = async (paymentId, payerId) => {
   try {
-    
-
     const payment = await new Promise((resolve, reject) => {
       paypal.payment.get(paymentId, function (error, payment) {
         if (error) {
@@ -68,6 +68,6 @@ exports.executePaypalPayment = async (paymentId, payerId) => {
 
     return payment;
   } catch (e) {
-    console.log(e)
+    console.log(e);
   }
-}
+};
